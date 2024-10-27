fedfb807be0d63403f405d5d89600c0d45040592
Niclas Overby <niclas@overby.me>
2024-10-27 20:47:00 +0100

    Add just

--- config/devShell.nix
+++ config/devShell.nix
@@ -12,7 +12,7 @@ inputs.devenv.lib.mkShell
       devenv.root = builtins.readFile inputs.devenv-root.outPath;
     }
     {
-      packages = with pkgs; [yarn nodejs];
+      packages = with pkgs; [yarn nodejs just];
     }
   ];
 }
