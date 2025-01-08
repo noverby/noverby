ab47c8a93e38048880c91d9825c211bb23f7044e
Niclas Overby <niclas@overby.me>
2025-01-08 15:06:23 +0100

    chore(config): Add GStreamer

--- config/home-modules/home.nix
+++ config/home-modules/home.nix
@@ -14,6 +14,10 @@
       DIRENV_LOG_FORMAT = "";
       PYTHONSTARTUP = "${homeDirectory}/.pystartup";
       GRANTED_ALIAS_CONFIGURED = "true";
+
+      # GStreamer
+      GST_PLUGIN_SYSTEM_PATH_1_0= with pkgs.gst_all_1; "${gstreamer.out}/lib/gstreamer-1.0:${gst-plugins-base}/lib/gstreamer-1.0:${gst-plugins-good}/lib/gstreamer-1.0";
+
       # XR
       XR_RUNTIME_JSON = "${pkgs.monado}/share/openxr/1/openxr_monado.json";
       XRT_COMPOSITOR_FORCE_XCB = "1";
