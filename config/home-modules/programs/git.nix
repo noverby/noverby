196cd363dbb21447e5c983301c70d7c716b9ff61
Niclas Overby <niclas@overby.me>
2025-02-27 13:15:32 +0100

    chore(config): switch to Mergiraf

--- config/home-modules/programs/git.nix
+++ config/home-modules/programs/git.nix
@@ -26,6 +26,10 @@
       };
       merge = {
         tool = "vi";
+        mergiraf = {
+          name = "mergiraf";
+          driver = "mergiraf merge --git %O %A %B -s %S -x %X -y %Y -p %P -l %L";
+        };
       };
       mergetool = {
         vi = {
@@ -53,5 +57,40 @@
         required = true;
       };
     };
+    attributes = [
+      "*.java merge=mergiraf"
+      "*.kt merge=mergiraf"
+      "*.rs merge=mergiraf"
+      "*.go merge=mergiraf"
+      "*.js merge=mergiraf"
+      "*.jsx merge=mergiraf"
+      "*.mjs merge=mergiraf"
+      "*.json merge=mergiraf"
+      "*.yml merge=mergiraf"
+      "*.yaml merge=mergiraf"
+      "*.toml merge=mergiraf"
+      "*.html merge=mergiraf"
+      "*.htm merge=mergiraf"
+      "*.xhtml merge=mergiraf"
+      "*.xml merge=mergiraf"
+      "*.c merge=mergiraf"
+      "*.h merge=mergiraf"
+      "*.cc merge=mergiraf"
+      "*.cpp merge=mergiraf"
+      "*.hpp merge=mergiraf"
+      "*.cs merge=mergiraf"
+      "*.dart merge=mergiraf"
+      "*.dts merge=mergiraf"
+      "*.scala merge=mergiraf"
+      "*.sbt merge=mergiraf"
+      "*.ts merge=mergiraf"
+      "*.tsx merge=mergiraf"
+      "*.py merge=mergiraf"
+      "*.php merge=mergiraf"
+      "*.phtml merge=mergiraf"
+      "*.sol merge=mergiraf"
+      "*.lua merge=mergiraf"
+      "*.rb merge=mergiraf"
+    ];
   };
 }
