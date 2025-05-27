657bfa66bcc885ee653c0392d60d251f3f029665
Niclas Overby <niclas@overby.me>
2025-05-27 14:16:11 +0200

    chore(nix/config): Remove gcr_4 override as it is not needed anymore

--- config/nixos-modules/packages.nix
+++ config/nixos-modules/packages.nix
@@ -1,37 +1,18 @@
-{
-  pkgs,
-  inputs,
-  ...
-}: {
+{...}: {
   nixpkgs.config = {
-    permittedInsecurePackages = [
-      "nix-2.24.5"
-    ];
-    packageOverrides = with inputs;
-      prev: {
-        #stardustxr = stardustxr.packages.${pkgs.system}.default;
-        flatland = flatland.packages.${pkgs.system}.default;
-        # monado = prev.monado.overrideAttrs (_: {
-        #   version = "unstable-2024-03-07";
-        #   src = prev.fetchFromGitHub {
-        #     owner = "noverby";
-        #     repo = "monado-mirror";
-        #     rev = "4bd442913eff792c4120b9195be6206a2d7a11cd";
-        #     hash = "sha256-sDGuNT2rmE25aRj3fP5hHUN76fUVqhLNebdv112cqpw=";
-        #   };
-        # });
-        gcr_4 = prev.gcr_4.overrideAttrs (prevPkg: {
-          nativeBuildInputs =
-            prevPkg.nativeBuildInputs
-            ++ [
-              prev.gnupg
-              prev.openssh
-            ];
-
-          mesonFlags = [
-            "-Dssh_agent=true"
-          ];
-        });
-      };
+    #packageOverrides = with inputs;
+    #  prev: {
+    #  stardustxr = stardustxr.packages.${pkgs.system}.default;
+    #  flatland = flatland.packages.${pkgs.system}.default;
+    #   monado = prev.monado.overrideAttrs (_: {
+    #     version = "unstable-2024-03-07";
+    #     src = prev.fetchFromGitHub {
+    #       owner = "noverby";
+    #       repo = "monado-mirror";
+    #       rev = "4bd442913eff792c4120b9195be6206a2d7a11cd";
+    #       hash = "sha256-sDGuNT2rmE25aRj3fP5hHUN76fUVqhLNebdv112cqpw=";
+    #     };
+    #   });
+    #  };
   };
 }
