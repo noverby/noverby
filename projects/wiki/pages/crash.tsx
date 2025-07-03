31e59c4aa2b722c8aef5f9d8e629516679ca6a02
Niclas Overby <niclas@overby.me>
2025-07-03 17:05:59 +0200

    fix(wiki): Escape functional linting

--- projects/wiki/pages/crash.tsx
+++ projects/wiki/pages/crash.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@mui/material';
 
 const Crash = () => {
+  // eslint-disable-next-line functional/no-throw-statements
   return <Button onClick={() => { throw Error("Triggered Crash") }}>Trigger Crash</Button>;
 };
 
