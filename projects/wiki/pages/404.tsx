978d75844b605e91df0e91a00c4f1ad9943da10d
Niclas Overby <niclas@overby.me>
2025-07-03 13:35:06 +0200

    chore(web/wiki): Force push path with static out

--- projects/wiki/pages/404.tsx
+++ projects/wiki/pages/404.tsx
@@ -1,8 +1,19 @@
-const P404 = () => (
-  <>
-    <p>404: Path not found!</p>
-    <p>This should not happen...</p>
-  </>
-);
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+const P404 = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push(router.asPath);
+  }, [router.asPath]);
+
+  return (
+    <>
+      <p>404: Path not found!</p>
+      <p>This should not happen...</p>
+    </>
+  );
+};
 
 export default P404;
