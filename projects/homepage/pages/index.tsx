1850104e1372b8e52ca946a6518e5c1658a8264d
Niclas Overby <niclas@overby.me>
2025-07-04 17:56:52 +0200

    chore(homepage): Clean with Biome

--- projects/homepage/pages/index.tsx
+++ projects/homepage/pages/index.tsx
@@ -1,18 +1,19 @@
-import dynamic from "next/dynamic";
-import { useEffect, useState } from "react";
-import Graph from "../components/Graph"
 import Head from "next/head";
+import { useEffect, useState } from "react";
+import Graph from "../components/Graph";
 
 export default function Index() {
-  const [showing, setShowing] = useState(false);
-  useEffect(() => {
-    setShowing(true);
-  }, []);
-  return (
-    <>
-      <Head><title>{"Niclas Overby Ⓝ"}</title></Head>
-      {showing && <Graph />}
-      <a rel="me" href="https://mas.to/@niclasoverby" />
-    </>
-  );
+	const [showing, setShowing] = useState(false);
+	useEffect(() => {
+		setShowing(true);
+	}, []);
+	return (
+		<>
+			<Head>
+				<title>{"Niclas Overby Ⓝ"}</title>
+			</Head>
+			{showing && <Graph />}
+			<a rel="me" href="https://mas.to/@niclasoverby" />
+		</>
+	);
 }
