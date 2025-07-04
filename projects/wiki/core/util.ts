855ef6a98b30e575269b93968a82972c06f44f5a
Niclas Overby <niclas@overby.me>
2025-07-04 21:22:20 +0200

    fix(wiki): Fix logic in checkVersion

--- projects/wiki/core/util.ts
+++ projects/wiki/core/util.ts
@@ -5,17 +5,17 @@ const checkVersion = () => {
 	switch (platform.layout) {
 		case "Gecko":
 			if (compare(platform.version ?? "0", "94", "<=")) return false;
-			break;
+			return true;
 		case "Blink":
 			if (
 				compare(platform.version ?? "0", "98", "<=") &&
 				platform.name !== "Opera"
 			)
 				return false;
-			break;
+			return true;
 		case "WebKit":
 			if (compare(platform.version ?? "0", "15.4", "<=")) return false;
-			break;
+			return true;
 		default:
 			return true;
 	}
