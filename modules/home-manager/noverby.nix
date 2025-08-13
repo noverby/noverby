1412b42dff8069693d1d39c7ed4deab5849081f3
Niclas Overby <niclas@overby.me>
2025-08-13 18:24:52 +0200

    chore(config): Add Catppuccin theme support to NixOS and Home Manager

--- modules/home-manager/noverby.nix
+++ modules/home-manager/noverby.nix
@@ -2,11 +2,13 @@
   imports = with inputs.self.homeModules; [
     inputs.zen-browser.homeModules.default
     inputs.spicetify-nix.homeManagerModules.spicetify
+    inputs.catppuccin.homeModules.catppuccin
     home
     systemd
     packages
     xdg
     file
     programs
+    catppuccin
   ];
 }
