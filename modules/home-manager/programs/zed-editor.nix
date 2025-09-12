aa5fe5a47e7c864ab8e612546dad6d10a8a89694
Niclas Overby <niclas@overby.me>
2025-09-12 22:32:11 +0200

    chore(config): Add Zed Television integration

--- modules/home-manager/programs/zed-editor.nix
+++ modules/home-manager/programs/zed-editor.nix
@@ -7,9 +7,11 @@
   configDir = "${config.xdg.configHome}/zed";
   settingsPath = "${configDir}/settings.json";
   keymapPath = "${configDir}/keymap.json";
+  tasksPath = "${configDir}/tasks.json";
 
   userKeymaps = builtins.fromJSON (builtins.readFile ./zed-editor/keymap.json);
   userSettings = builtins.fromJSON (builtins.readFile ./zed-editor/settings.json);
+  userTasks = builtins.fromJSON (builtins.readFile ./zed-editor/tasks.json);
 in {
   programs.zed-editor = {
     enable = true;
@@ -30,12 +32,14 @@ in {
       overwriteZedSymlink = let
         jsonSettings = pkgs.writeText "tmp_zed_settings" (builtins.toJSON userSettings);
         jsonKeymaps = pkgs.writeText "tmp_zed_keymaps" (builtins.toJSON userKeymaps);
+        jsonTasks = pkgs.writeText "tmp_zed_tasks" (builtins.toJSON userTasks);
       in
         lib.hm.dag.entryAfter ["linkGeneration"] ''
           mkdir -p "${configDir}"
           rm -rf "${settingsPath}" "${keymapPath}"
           cat ${jsonSettings} | ${pkgs.jq}/bin/jq --monochrome-output > "${settingsPath}"
           cat ${jsonKeymaps} | ${pkgs.jq}/bin/jq --monochrome-output > "${keymapPath}"
+          cat ${jsonTasks} | ${pkgs.jq}/bin/jq --monochrome-output > "${tasksPath}"
           chmod u+w "${settingsPath}" "${keymapPath}"
         '';
     };
