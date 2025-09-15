a1bf0c312055dc81899e65cfe2b8bb7ef920e18d
Niclas Overby <niclas@overby.me>
2025-09-15 11:15:30 +0200

    fix(nix/config): Add missing dmcrypt dependency cryptsetup

--- modules/nixos/veo/default.nix
+++ modules/nixos/veo/default.nix
@@ -2,7 +2,7 @@
   systemd.services.dmcryptd = {
     description = "DMcrypt daemon";
     wantedBy = ["multi-user.target"];
-    path = with pkgs; [util-linux];
+    path = with pkgs; [util-linux cryptsetup];
     serviceConfig = {
       ExecStart = "${pkgs.python3}/bin/python3 ${./dmcryptd.py}";
       # You may also want to add these common settings:
