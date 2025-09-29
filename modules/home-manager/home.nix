2dba612e8fe81b54406996c3a69d00f74037995b
Niclas Overby <niclas@overby.me>
2025-09-29 15:57:15 +0200

    chore(config): Remove unused env vars

--- modules/home-manager/home.nix
+++ modules/home-manager/home.nix
@@ -61,19 +61,8 @@
       EDITOR = "vi";
       VISUAL = "vi";
       BATDIFF_USE_DELTA = "true";
-      DIRENV_LOG_FORMAT = "";
       PYTHONSTARTUP = "${homeDirectory}/.pystartup";
       GRANTED_ALIAS_CONFIGURED = "true";
-
-      # GStreamer
-      GST_PLUGIN_SYSTEM_PATH_1_0 = with pkgs.gst_all_1; "${gstreamer.out}/lib/gstreamer-1.0:${gst-plugins-base}/lib/gstreamer-1.0:${gst-plugins-good}/lib/gstreamer-1.0";
-
-      # XR
-      XR_RUNTIME_JSON = "${pkgs.monado}/share/openxr/1/openxr_monado.json";
-      XRT_COMPOSITOR_FORCE_XCB = "1";
-      XRT_COMPOSITOR_XCB_FULLSCREEN = "1";
-
-      DEVENV_ENABLE_HOOKS = "1";
     };
   };
 }
