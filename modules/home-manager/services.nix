0e42df25533cd6852b8f3b2b0214a53e20c5aba8
Niclas Overby <niclas@overby.me>
2025-09-29 17:35:47 +0200

    fix(nix/config): Add useable ssh-agent

--- modules/home-manager/services.nix
+++ modules/home-manager/services.nix
@@ -1,5 +1,11 @@
 _: {
   services = {
+    ssh-agent = {
+      enable = true;
+      # Cosmic has hardcoded socket path
+      # https://github.com/pop-os/cosmic-session/blob/379ce30715f637075879feda784edc89231792cf/data/start-cosmic#L58
+      socket = "keyring/ssh";
+    };
     # Blocked by: https://github.com/NixOS/nixpkgs/pull/423931
     espanso = {
       enable = true;
