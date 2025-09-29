febc8027be0c282fa886393d224289b3e0a6db30
Niclas Overby <niclas@overby.me>
2025-09-29 15:51:07 +0200

    chore(nix/pkgs): Use master branch

--- packages/envy.nix
+++ packages/envy.nix
@@ -10,13 +10,13 @@ rustPlatform.buildRustPackage rec {
   src = fetchFromGitHub {
     owner = "mre";
     repo = "envy";
-    rev = "bash-support";
-    hash = "sha256-EJc40+1RYf6mUQbFhcYARrj6ERL5jRtPdYL62bj9zHw=";
+    rev = "master";
+    hash = "sha256-33efk7Udkchy2bm9hjcjNlUxCNFkaVJQG/U3h/vp/2g=";
   };
 
   buildFeatures = ["bash-support"];
 
-  cargoHash = "sha256-lk9LOg1vVus6i5d1tcT8FWWchrLDlLXpht9xESfDpNM=";
+  cargoHash = "sha256-L4Pm57GzcbC7bWafv33g3G3hD2vtcBu1L7Pkvuox4mY=";
 
   doCheck = false;
 
