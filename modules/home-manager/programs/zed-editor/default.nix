635114214519802f94878ea2ba0a8e13379d1949
Niclas Overby <niclas@overby.me>
2025-10-19 22:06:27 +0200

    chore(config): Simplify Zed config logic

--- modules/home-manager/programs/zed-editor/default.nix
+++ modules/home-manager/programs/zed-editor/default.nix
@@ -3,16 +3,7 @@
   lib,
   pkgs,
   ...
-}: let
-  configDir = "${config.xdg.configHome}/zed";
-  settingsPath = "${configDir}/settings.json";
-  keymapPath = "${configDir}/keymap.json";
-  tasksPath = "${configDir}/tasks.json";
-
-  userKeymaps = builtins.fromJSON (builtins.readFile ./keymap.json);
-  userSettings = builtins.fromJSON (builtins.readFile ./settings.json);
-  userTasks = builtins.fromJSON (builtins.readFile ./tasks.json);
-in {
+}: {
   programs.zed-editor = {
     enable = true;
     extensions = [
@@ -31,24 +22,26 @@ in {
     ];
   };
   home = {
-    activation = {
+    activation = let
+      configDir = "${config.xdg.configHome}/zed";
+      settingsPath = "${configDir}/settings.json";
+      keymapPath = "${configDir}/keymap.json";
+      tasksPath = "${configDir}/tasks.json";
+
+      userKeymaps = builtins.readFile ./keymap.json;
+      userSettings = builtins.readFile ./settings.json;
+      userTasks = builtins.readFile ./tasks.json;
+    in {
       removeExistingZedSettings = lib.hm.dag.entryBefore ["checkLinkTargets"] ''
         rm -rf "${settingsPath}" "${keymapPath}"
       '';
 
-      overwriteZedSymlink = let
-        jsonSettings = pkgs.writeText "tmp_zed_settings" (builtins.toJSON userSettings);
-        jsonKeymaps = pkgs.writeText "tmp_zed_keymaps" (builtins.toJSON userKeymaps);
-        jsonTasks = pkgs.writeText "tmp_zed_tasks" (builtins.toJSON userTasks);
-      in
-        lib.hm.dag.entryAfter ["linkGeneration"] ''
-          mkdir -p "${configDir}"
-          rm -rf "${settingsPath}" "${keymapPath}"
-          cat ${jsonSettings} | ${pkgs.jq}/bin/jq --monochrome-output > "${settingsPath}"
-          cat ${jsonKeymaps} | ${pkgs.jq}/bin/jq --monochrome-output > "${keymapPath}"
-          cat ${jsonTasks} | ${pkgs.jq}/bin/jq --monochrome-output > "${tasksPath}"
-          chmod u+w "${settingsPath}" "${keymapPath}"
-        '';
+      overwriteZedSymlink = lib.hm.dag.entryAfter ["linkGeneration"] ''
+        mkdir -p "${configDir}"
+        cat ${pkgs.writeText "zed-settings" userSettings} > "${settingsPath}"
+        cat ${pkgs.writeText "zed-keymaps" userKeymaps} > "${keymapPath}"
+        cat ${pkgs.writeText "zed-tasks" userTasks} > "${tasksPath}"
+      '';
     };
   };
 }
