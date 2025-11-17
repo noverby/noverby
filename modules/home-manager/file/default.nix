66c28f2fff9b2aee058bbe63681ad14870cd9942
Niclas Overby <niclas@overby.me>
2025-11-17 14:36:32 +0100

    chore(nix/config): Move ~/.local/bin to profile bins

--- modules/home-manager/file/default.nix
+++ modules/home-manager/file/default.nix
@@ -5,43 +5,42 @@
   ...
 }:
 with config.lib.file; {
-  home.file = {
-    Pictures.source = mkOutOfStoreSymlink "${homeDirectory}/Sync/Pictures";
-    Documents.source = mkOutOfStoreSymlink "${homeDirectory}/Sync/Documents";
-    Desktop.source = mkOutOfStoreSymlink "${homeDirectory}/Sync/Desktop";
-    Videos.source = mkOutOfStoreSymlink "${homeDirectory}/Sync/Videos";
-    Music.source = mkOutOfStoreSymlink "${homeDirectory}/Sync/Music";
-    Templates.source = mkOutOfStoreSymlink "${homeDirectory}/Sync/Templates";
-    "Work/proj".source = mkOutOfStoreSymlink "${homeDirectory}/Sync/Projects";
-    "Work/wiki".source = mkOutOfStoreSymlink "${homeDirectory}/Sync/Documents/Wiki";
-    "Work/tmp/.keep".source = builtins.toFile "keep" "";
-    ".ssh/socket/.keep".source = builtins.toFile "keep" "";
-    ".local/bin/vi" = {
-      executable = true;
-      source = ./bin/vi;
+  home = {
+    packages = [
+      (
+        pkgs.writeShellScriptBin "vi" ./bin/vi
+      )
+      (
+        pkgs.writeShellScriptBin "uf" ./bin/uf
+      )
+      (
+        pkgs.writeShellScriptBin "zed-uf" ./bin/zed-uf
+      )
+      (
+        pkgs.writeShellScriptBin "zellij-cwd" ./bin/zellij-cwd
+      )
+      (
+        pkgs.writeShellScriptBin "nix-flamegraph" ./bin/nix-flamegraph
+      )
+    ];
+    file = {
+      Pictures.source = mkOutOfStoreSymlink "${homeDirectory}/Sync/Pictures";
+      Documents.source = mkOutOfStoreSymlink "${homeDirectory}/Sync/Documents";
+      Desktop.source = mkOutOfStoreSymlink "${homeDirectory}/Sync/Desktop";
+      Videos.source = mkOutOfStoreSymlink "${homeDirectory}/Sync/Videos";
+      Music.source = mkOutOfStoreSymlink "${homeDirectory}/Sync/Music";
+      Templates.source = mkOutOfStoreSymlink "${homeDirectory}/Sync/Templates";
+      "Work/proj".source = mkOutOfStoreSymlink "${homeDirectory}/Sync/Projects";
+      "Work/wiki".source = mkOutOfStoreSymlink "${homeDirectory}/Sync/Documents/Wiki";
+      "Work/tmp/.keep".source = builtins.toFile "keep" "";
+      ".ssh/socket/.keep".source = builtins.toFile "keep" "";
+      ".npmrc".source = ./config/npmrc.ini;
+      ".config/pop-shell/config.json".source = ./config/pop-shell/config.json;
+      ".config/mpv/mpv.conf".source = ./config/mpv/mpv.conf;
+      ".local/share/wallpapers/current.png".source = "${(pkgs.nix-wallpaper.override {
+        preset = "catppuccin-mocha";
+        logoSize = 10;
+      })}/share/wallpapers/nixos-wallpaper.png";
     };
-    ".local/bin/uf" = {
-      executable = true;
-      source = ./bin/uf;
-    };
-    ".local/bin/zed-uf" = {
-      executable = true;
-      source = ./bin/zed-uf;
-    };
-    ".local/bin/zellij-cwd" = {
-      executable = true;
-      source = ./bin/zellij-cwd;
-    };
-    ".local/bin/nix-flamegraph" = {
-      executable = true;
-      source = ./bin/nix-flamegraph;
-    };
-    ".npmrc".source = ./config/npmrc.ini;
-    ".config/pop-shell/config.json".source = ./config/pop-shell/config.json;
-    ".config/mpv/mpv.conf".source = ./config/mpv/mpv.conf;
-    ".local/share/wallpapers/current.png".source = "${(pkgs.nix-wallpaper.override {
-      preset = "catppuccin-mocha";
-      logoSize = 10;
-    })}/share/wallpapers/nixos-wallpaper.png";
   };
 }
