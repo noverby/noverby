f32785f9b8f606f5b41aa56aaac5f59152a51739
Niclas Overby <niclas@overby.me>
2025-11-28 12:53:38 +0100

    chore(nix/config): Add PATH to systemd user environment for Zed URL handling

--- modules/nixos/cosmic.nix
+++ modules/nixos/cosmic.nix
@@ -21,4 +21,8 @@
     displayManager.cosmic-greeter.enable = true;
     system76-scheduler.enable = true;
   };
+  # Fix Zed open urls: https://github.com/NixOS/nixpkgs/issues/189851#issuecomment-1759954096
+  systemd.user.extraConfig = ''
+    DefaultEnvironment="PATH=/run/wrappers/bin:/etc/profiles/per-user/%u/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin"
+  '';
 }
