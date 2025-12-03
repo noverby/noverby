fedead960345e1df265107647bfebe2ee2e278a2
Niclas Overby <niclas@overby.me>
2025-12-03 11:21:16 +0100

    fix(config): Make Zed able to load direnv env again

--- modules/nixos/base.nix
+++ modules/nixos/base.nix
@@ -188,6 +188,7 @@
   # Users
   environment.profiles = ["$HOME/.local"];
   users.users.noverby = {
+    shell = pkgs.nushell;
     isNormalUser = true;
     description = "Niclas Overby";
     extraGroups = ["networkmanager" "wheel" "docker" "libvirtd" "wireshark"];
