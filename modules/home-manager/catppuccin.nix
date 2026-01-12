cf514122234903d5bd231748e81327e2fe0059e4
Niclas Overby <niclas@overby.me>
2026-01-12 14:22:05 +0100

    feat(nix/config): Enable Catppuccin for Qt

--- modules/home-manager/catppuccin.nix
+++ modules/home-manager/catppuccin.nix
@@ -1,5 +1,12 @@
 _: {
+  qt = {
+    enable = true;
+    style.name = "qtct";
+    platformTheme.name = "qtct";
+  };
   catppuccin = {
     enable = true;
+    qt5ct.enable = true;
+    kvantum.enable = false;
   };
 }
