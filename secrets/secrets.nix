cc61bf0a4f6b01e33b026d08b519d2a1afed99f6
Niclas Overby <niclas@overby.me>
2026-01-13 11:07:25 +0100

    feat(config): Manage Age with Nitrokey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,7 @@
 let
   noverby = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOachAYzBH8Qaorvbck99Fw+v6md3BeVtfL5PJ/byv4Cc";
+  nitrokey3-fido2-hmac = "age1efazxe5tgepdv5czxzj5x844dj265dar4sej42qc8mjp2czulazslqutuw";
 in {
-  "resolved.age".publicKeys = [noverby];
-  "u2f-keys.age".publicKeys = [noverby];
+  "resolved.age".publicKeys = [noverby nitrokey3-fido2-hmac];
+  "u2f-keys.age".publicKeys = [noverby nitrokey3-fido2-hmac];
 }
