592ef525e8d7ae1da7b2dcd32396efe1deef10d6
Niclas Overby <niclas@overby.me>
2026-01-13 15:37:25 +0100

    chore(nix/config): Support arbiary number of users

--- modules/nixos/home-manager.nix
+++ modules/nixos/home-manager.nix
@@ -1,16 +1,27 @@
 {
   pkgs,
+  lib,
   inputs,
   stateVersion,
+  src,
   ...
 }: let
   username = "noverby";
   homeDirectory = "/home/${username}";
+  usersPath = src + /modules/users;
+  users = builtins.listToAttrs (
+    map (
+      file: {
+        name = lib.removeSuffix ".nix" file;
+        value = usersPath + "/${file}";
+      }
+    ) (builtins.attrNames (builtins.readDir usersPath))
+  );
 in {
   home-manager = {
+    inherit users;
     useGlobalPkgs = false;
     useUserPackages = true;
-    users.noverby = inputs.self.homeModules.noverby;
     extraSpecialArgs = {
       inherit inputs pkgs username homeDirectory stateVersion;
     };
