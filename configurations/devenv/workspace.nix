04076abc2528fe6e05fdb2970bb73c4f41958e1f
Niclas Overby <niclas@overby.me>
2026-01-15 13:53:56 +0100

    chore(config): Move devenv-root to module

--- configurations/devenv/workspace.nix
+++ configurations/devenv/workspace.nix
@@ -1,58 +1,50 @@
 {
   pkgs,
   inputs,
-}: let
-  envJson = builtins.readFile inputs.env.outPath;
-  env =
-    if envJson != ""
-    then builtins.fromJSON envJson
-    else {PWD = "/home/noverby/Work/noverby";};
-in
-  inputs.devenv.lib.mkShell
-  {
-    inherit inputs pkgs;
+}:
+inputs.devenv.lib.mkShell
+{
+  inherit inputs pkgs;
 
-    modules = with inputs.self.devenvModules; [
-      git-hooks
-      {
-        devenv.root = env.PWD;
-      }
-      {
-        languages = {
-          rust = {
-            enable = true;
-          };
+  modules = with inputs.self.devenvModules; [
+    devenv-root
+    git-hooks
+    {
+      languages = {
+        rust = {
+          enable = true;
         };
+      };
 
-        packages = with pkgs; [
-          # IDE
-          harper
-          # Common
-          just
-          rumdl
-          # Nix
-          nixd
-          nil
-          alejandra
-          (writeShellScriptBin "ragenix" ''
-            exec ${ragenix}/bin/ragenix -i ~/.age/id_fido2 "$@"
-          '')
-          # Rust
-          openssl
-          # Mojo
-          mojo
-          python3
-          llvmPackages_latest.llvm
-          llvmPackages_latest.lld
-          # Deno
-          deno
-          # Media
-          cavif-rs
-          presenterm
-          python313Packages.weasyprint
-          # DevOps
-          scaleway-cli
-        ];
-      }
-    ];
-  }
+      packages = with pkgs; [
+        # IDE
+        harper
+        # Common
+        just
+        rumdl
+        # Nix
+        nixd
+        nil
+        alejandra
+        (writeShellScriptBin "ragenix" ''
+          exec ${ragenix}/bin/ragenix -i ~/.age/id_fido2 "$@"
+        '')
+        # Rust
+        openssl
+        # Mojo
+        mojo
+        python3
+        llvmPackages_latest.llvm
+        llvmPackages_latest.lld
+        # Deno
+        deno
+        # Media
+        cavif-rs
+        presenterm
+        python313Packages.weasyprint
+        # DevOps
+        scaleway-cli
+      ];
+    }
+  ];
+}
