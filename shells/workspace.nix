a2e8b3603f4c887b091d9dd43696eeea644fef88
Niclas Overby <niclas@overby.me>
2026-01-15 12:15:02 +0100

    feat(config): Allow loading workspace without impure

--- shells/workspace.nix
+++ shells/workspace.nix
@@ -1,87 +1,96 @@
 {
   pkgs,
   inputs,
-}:
-inputs.devenv.lib.mkShell
-{
-  inherit inputs pkgs;
+}: let
+  envJson = builtins.readFile inputs.env.outPath;
+  env =
+    if envJson != ""
+    then builtins.fromJSON envJson
+    else {PWD = "/home/noverby/Work/noverby";};
+in
+  inputs.devenv.lib.mkShell
+  {
+    inherit inputs pkgs;
 
-  modules = [
-    {
-      git-hooks = {
-        package = pkgs.prek;
-        hooks = {
-          denolint.enable = true;
-          biome.enable = true;
-          alejandra.enable = true;
-          statix.enable = true;
-          typos.enable = true;
-          rustfmt.enable = true;
-          rumdl.enable = true;
-          nil = {
-            enable = true;
-            entry = builtins.toString (pkgs.writeShellScript "precommit-nil" ''
-              errors=false
-              echo Checking: $@
-              for file in $(echo "$@"); do
-                ${pkgs.nil}/bin/nil diagnostics --deny-warnings "$file"
-                exit_code=$?
+    modules = [
+      {
+        devenv.root = env.PWD;
+      }
+      {
+        git-hooks = {
+          package = pkgs.prek;
+          hooks = {
+            denolint.enable = true;
+            biome.enable = true;
+            alejandra.enable = true;
+            statix.enable = true;
+            typos.enable = true;
+            rustfmt.enable = true;
+            rumdl.enable = true;
+            nil = {
+              enable = true;
+              entry = builtins.toString (pkgs.writeShellScript "precommit-nil" ''
+                errors=false
+                echo Checking: $@
+                for file in $(echo "$@"); do
+                  ${pkgs.nil}/bin/nil diagnostics --deny-warnings "$file"
+                  exit_code=$?
 
-                if [[ $exit_code -ne 0 ]]; then
-                  echo \"$file\" failed with exit code: $exit_code
-                  errors=true
+                  if [[ $exit_code -ne 0 ]]; then
+                    echo \"$file\" failed with exit code: $exit_code
+                    errors=true
+                  fi
+                done
+                if [[ $errors == true ]]; then
+                  exit 1
                 fi
-              done
-              if [[ $errors == true ]]; then
-                exit 1
-              fi
-            '');
+              '');
+            };
+            commitlint-rs = {
+              enable = true;
+              package = pkgs.commitlint-rs;
+              name = "prepare-commit-msg-commitlint-rs";
+              entry = "${pkgs.commitlint-rs}/bin/commitlint --edit";
+              stages = ["prepare-commit-msg"];
+            };
           };
-          commitlint-rs = {
+        };
+
+        languages = {
+          rust = {
             enable = true;
-            package = pkgs.commitlint-rs;
-            name = "prepare-commit-msg-commitlint-rs";
-            entry = "${pkgs.commitlint-rs}/bin/commitlint --edit";
-            stages = ["prepare-commit-msg"];
           };
         };
-      };
 
-      languages = {
-        rust = {
-          enable = true;
-        };
-      };
-
-      packages = with pkgs; [
-        # IDE
-        harper
-        # Common
-        just
-        rumdl
-        # Nix
-        nixd
-        nil
-        alejandra
-        (writeShellScriptBin "ragenix" ''
-          exec ${ragenix}/bin/ragenix -i ~/.age/id_fido2 "$@"
-        '')
-        # Rust
-        openssl
-        # Mojo
-        mojo
-        python3
-        llvmPackages_latest.llvm
-        llvmPackages_latest.lld
-        # Deno
-        deno
-        # Media
-        cavif-rs
-        presenterm
-        python313Packages.weasyprint
-        # DevOps
-        scaleway-cli
-      ];
-    }
-  ];
-}
+        packages = with pkgs; [
+          # IDE
+          harper
+          # Common
+          just
+          rumdl
+          # Nix
+          nixd
+          nil
+          alejandra
+          (writeShellScriptBin "ragenix" ''
+            exec ${ragenix}/bin/ragenix -i ~/.age/id_fido2 "$@"
+          '')
+          # Rust
+          openssl
+          # Mojo
+          mojo
+          python3
+          llvmPackages_latest.llvm
+          llvmPackages_latest.lld
+          # Deno
+          deno
+          # Media
+          cavif-rs
+          presenterm
+          python313Packages.weasyprint
+          # DevOps
+          scaleway-cli
+        ];
+      }
+    ];
+  }
