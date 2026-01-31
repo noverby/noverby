7b0a5a5f5be92b34c48a016c87346917a3b0603b
Niclas Overby <niclas@overby.me>
2026-01-31 18:22:31 +0100

    fix(web/wiki): Dump files

--- projects/wiki/rsbuild.config.ts
+++ projects/wiki/rsbuild.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "@rsbuild/core";
 import { pluginNodePolyfill } from "@rsbuild/plugin-node-polyfill";
 import { pluginReact } from "@rsbuild/plugin-react";
+import { pluginSvgr } from '@rsbuild/plugin-svgr';
 
 export default defineConfig({
-	plugins: [pluginReact(), pluginNodePolyfill()],
+	plugins: [pluginReact(), pluginNodePolyfill(), pluginSvgr()],
 	html: {
 		template: "./public/index.html",
 	},
