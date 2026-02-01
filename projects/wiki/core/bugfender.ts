473094122130aa88d498bf2e501d9a4c6c897f22
Niclas Overby <niclas@overby.me>
2026-02-01 16:01:04 +0100

    chore(web/wiki): Update Bugfender env var to PUBLIC_BUGFENDER_APP_KEY

--- projects/wiki/core/bugfender.ts
+++ projects/wiki/core/bugfender.ts
@@ -4,11 +4,11 @@ export const initBugfender = async () => {
 	if (
 		typeof window !== "undefined" &&
 		!isInitialized &&
-		process.env.NEXT_PUBLIC_BUGFENDER_APP_KEY
+		process.env.PUBLIC_BUGFENDER_APP_KEY
 	) {
 		const { Bugfender } = await import("@bugfender/sdk");
 		Bugfender.init({
-			appKey: process.env.NEXT_PUBLIC_BUGFENDER_APP_KEY,
+			appKey: process.env.PUBLIC_BUGFENDER_APP_KEY,
 			// Override console methods to capture all console.log, console.error, etc.
 			overrideConsoleMethods: true,
 			// Print logs to browser console as well (useful for development)
