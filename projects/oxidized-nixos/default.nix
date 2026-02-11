c64f8e0aa54a87c2491033924ecc55bc3bf51aa0
Niclas Overby <niclas@overby.me>
2026-02-11 22:04:54 +0100

    feat(nixos-rs): Add Oxidized NixOS project
    Add new project for an oxidized NixOS VM, including default.nix and
    justfile for image building and running with cloud-hypervisor. Update
    .gitignore for VM image files. Register project in flake.nix.

---
+++ projects/oxidized-nixos/default.nix
@@ -0,0 +1,38 @@
+{
+  nixosConfigurations.oxidized-nixos = {
+    inputs,
+    lib,
+    ...
+  }: {
+    system = "x86_64-linux";
+    modules = [
+      ({modulesPath, ...}: {
+        imports = ["${modulesPath}/profiles/qemu-guest.nix"];
+
+        system = {
+          name = "oxidized";
+          stateVersion = "25.11";
+        };
+
+        boot = {
+          kernelParams = ["init=/nix/var/nix/profiles/system/init"];
+          loader.grub.enable = false;
+        };
+
+        fileSystems."/" = {
+          device = "/dev/disk/by-label/nixos";
+          autoResize = true;
+          fsType = "ext4";
+        };
+
+        users.users = {
+          oxidized = {
+            isNormalUser = true;
+            extraGroups = ["wheel"];
+            password = "oxidized";
+          };
+        };
+      })
+    ];
+  };
+}
