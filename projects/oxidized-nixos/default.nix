54c0a2c89c42db42859ce50e6b962a844b03ee65
Niclas Overby <niclas@overby.me>
2026-02-12 22:09:27 +0100

    feat(nixos-rs): Replace coreutils with uutils-coreutils

--- projects/oxidized-nixos/default.nix
+++ projects/oxidized-nixos/default.nix
@@ -12,7 +12,12 @@
   }: {
     system = "x86_64-linux";
     modules = [
-      ({modulesPath, ...}: {
+      ({
+        modulesPath,
+        pkgs,
+        lib,
+        ...
+      }: {
         imports = ["${modulesPath}/profiles/qemu-guest.nix"];
 
         system = {
@@ -38,6 +43,19 @@
             password = "oxidized";
           };
         };
+
+        system.replaceDependencies.replacements = let
+          uutils = pkgs.uutils-coreutils-noprefix;
+        in [
+          {
+            original = pkgs.coreutils;
+            replacement = uutils.overrideAttrs {name = "coreutils-9.8";};
+          }
+          {
+            original = pkgs.coreutils-full;
+            replacement = uutils.overrideAttrs {name = "coreutils-full-9.8";};
+          }
+        ];
 
         services.getty.autologinUser = "oxidized";
         security.sudo.enable = false;
