25b9a400bae02d259b45f3d3c9cd1a7bb529d673
Niclas Overby <niclas@overby.me>
2026-02-14 20:52:42 +0100

    chore(config): Update nix/modules

--- nix/modules/home-manager/programs/starship.nix
+++ nix/modules/home-manager/programs/starship.nix
@@ -1,19 +1,15 @@
-{
+{pkgs, ...}: {
   programs.starship = {
     enable = true;
     settings = {
-      command_timeout = 10000;
-      time = {
-        disabled = false;
-        format = " [$time]($style) ";
-      };
-      status = {
-        disabled = false;
-      };
-      directory = {
-        truncation_length = 8;
-        truncation_symbol = ".../";
-        truncate_to_repo = false;
+      format = "$all\${custom.jj}";
+      custom.jj = {
+        command = "prompt";
+        format = "$output";
+        ignore_timeout = true;
+        shell = [pkgs.starship-jj "--ignore-working-copy" "starship"];
+        use_stdin = false;
+        "when" = true;
       };
     };
   };
