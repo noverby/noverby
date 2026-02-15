4e3111a6b119c8caa72ada0ee6984d481fec854b
Niclas Overby <niclas@overby.me>
2026-02-15 08:54:44 +0100

    feat(rustysd): Add colored log level output

--- projects/rustysd/src/logging.rs
+++ projects/rustysd/src/logging.rs
@@ -1,11 +1,19 @@
 pub fn setup_logging(conf: &crate::config::LoggingConfig) -> Result<(), String> {
     let mut logger = fern::Dispatch::new()
         .format(|out, message, record| {
+            let level = record.level();
+            let colored_level = match level {
+                log::Level::Error => format!("\x1b[31m{}\x1b[0m", level),
+                log::Level::Warn => format!("\x1b[33m{}\x1b[0m", level),
+                log::Level::Info => format!("\x1b[32m{}\x1b[0m", level),
+                log::Level::Debug => format!("\x1b[34m{}\x1b[0m", level),
+                log::Level::Trace => format!("\x1b[36m{}\x1b[0m", level),
+            };
             out.finish(format_args!(
                 "{}[{}][{}] {}",
                 chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
                 record.target(),
-                record.level(),
+                colored_level,
                 message
             ));
         })
