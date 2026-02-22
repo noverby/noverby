7e519bce62b70fd2046a81617f9385e71080f455
Niclas Overby <niclas@overby.me>
2026-02-22 18:28:44 +0100

    chore(nixos): Simplify PAGER to use tailspin directly

--- nixos/core/environment.nix
+++ nixos/core/environment.nix
@@ -1,20 +1,11 @@
-{pkgs, ...}: let
-  tspin-pager = pkgs.writeShellScriptBin "tspin-pager" ''
-    if [ -t 0 ] && [ -t 1 ]; then
-      exec ${pkgs.tailspin}/bin/tspin "$@"
-    else
-      exec ${pkgs.tailspin}/bin/tspin --print "$@"
-    fi
-  '';
-in {
+{pkgs, ...}: {
   environment = {
     systemPackages = with pkgs; [
       helix
       tailspin
-      tspin-pager
     ];
     sessionVariables = {
-      PAGER = "tspin-pager";
+      PAGER = "${pkgs.tailspin}/bin/tspin";
       SYSTEMD_PAGERSECURE = "1";
       NIXOS_OZONE_WL = "1";
     };
