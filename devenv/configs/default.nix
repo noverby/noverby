818b266a46997df5530496e2e2f6ea0c37b72055
Niclas Overby <niclas@overby.me>
2026-02-22 15:03:40 +0100

    chore(devenv): Move rules from .zed/rules.md to .rules

--- devenv/configs/default.nix
+++ devenv/configs/default.nix
@@ -8,6 +8,7 @@
     ln -sf ${./typos.toml} typos.toml
     ln -sf ${./secretsignore} .secretsignore
     ln -sf ${./commitlintrc.yml} .commitlintrc.yml
-    mkdir -p .zed && cp -f ${./zed/settings.jsonc} .zed/settings.json && cp -f ${./zed/rules.md} .zed/rules.md
+    mkdir -p .zed && cp -f ${./zed/settings.jsonc} .zed/settings.json
+    cp -f ${./zed/rules.md} .rules
   '';
 }
