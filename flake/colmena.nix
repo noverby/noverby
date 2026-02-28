def2f01c1510891f9bbbfedc1a27667ec09c9ab5
Niclas Overby <niclas@overby.me>
2026-02-28 23:10:35 +0100

    feat(flake.nix): Add colmena deployment and fix nixosConfigurations alias
    Signed-off-by: Niclas Overby <niclas@overby.me>

---
+++ flake/colmena.nix
@@ -0,0 +1,47 @@
+{
+  config,
+  inputs,
+  lib,
+  ...
+}: let
+  isNixos = x: x ? config.system.build.toplevel;
+
+  standardConfigs =
+    lib.filterAttrs (_: cfg: !(isNixos cfg)) config.nixosConfigurations;
+in {
+  outputs.colmena =
+    {
+      meta = {
+        nixpkgs = import inputs.nixpkgs {
+          system = "x86_64-linux";
+          config.allowUnfree = true;
+        };
+        nodeNixpkgs = builtins.mapAttrs (_: cfg:
+          import inputs.nixpkgs {
+            inherit (cfg) system;
+            config.allowUnfree = true;
+          })
+        standardConfigs;
+        nodeSpecialArgs = builtins.mapAttrs (name: cfg:
+          {
+            inherit inputs;
+            hostname = name;
+          }
+          // (cfg.specialArgs or {}))
+        standardConfigs;
+      };
+    }
+    // builtins.mapAttrs (name: cfg: {
+      deployment = {
+        targetHost = "${name}.overby.me";
+        targetUser = "root";
+      };
+      imports =
+        [
+          config.propagationModule
+          ({flake, ...}: {_module.args = {inherit (flake) inputs';};})
+        ]
+        ++ (cfg.modules or []);
+    })
+    standardConfigs;
+}
