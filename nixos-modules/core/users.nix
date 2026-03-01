dcddacb6ce8b8ebfd85b705f1270dce37195e427
Niclas Overby <niclas@overby.me>
2026-03-01 09:28:13 +0100

    feat(nixos-modules): Add noverby-ssh-ed25519 to authorized SSH keys

--- nixos-modules/core/users.nix
+++ nixos-modules/core/users.nix
@@ -1,9 +1,16 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  src,
+  ...
+}: let
+  publicKeys = import "${src}/config/secrets/publicKeys.nix";
+in {
   environment.profiles = ["$HOME/.local"];
   users.users.noverby = {
     shell = pkgs.nushell;
     isNormalUser = true;
     description = "Niclas Overby";
     extraGroups = ["networkmanager" "wheel" "docker" "libvirtd" "wireshark" "input" "kvm"];
+    openssh.authorizedKeys.keys = [publicKeys.noverby-ssh-ed25519];
   };
 }
