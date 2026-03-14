8a06990b4ff654c079bf2b2dcec2b4ad74f8d006
Niclas Overby <niclas@overby.me>
2026-03-14 15:20:22 +0100

    feat(web/homepage): Add Nix package definition using buildDenoProject
    Signed-off-by: Niclas Overby <niclas@overby.me>

--- homepage/default.nix
+++ homepage/default.nix
@@ -9,4 +9,23 @@
       deno
     ];
   };
+
+  packages.homepage-frontend = {lib, ...}:
+    lib.buildDenoProject {
+      pname = "homepage-frontend";
+      src = lib.fileset.toSource {
+        root = ./.;
+        fileset = lib.fileset.unions [
+          ./package.json
+          ./deno.lock
+          ./tsconfig.json
+          ./rsbuild.config.ts
+          ./src
+          ./public
+        ];
+      };
+      buildCommand = "deno run -A npm:@rsbuild/core build";
+      installPhase = "cp -r dist $out";
+      meta.description = "Homepage frontend built with Deno + RSBuild";
+    };
 }
