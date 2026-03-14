6ff7dcc2e211b0c35537b908a836c22c8043ff10
Niclas Overby <niclas@overby.me>
2026-03-14 08:41:16 +0100

    feat(config): Switch home to tangled-spindle-nix with ragenix token
    Signed-off-by: Niclas Overby <niclas@overby.me>

--- config/nixos/home.nix
+++ config/nixos/home.nix
@@ -9,7 +9,7 @@
   specialArgs = {
     inherit src inputs lib;
     stateVersion = "25.05";
-    hasSecrets = false;
+    hasSecrets = true;
   };
 
   modules = with inputs.self.nixosModules; [
@@ -18,14 +18,42 @@
     inputs.self.hardware.dell-xps-9320
     inputs.self.desktops.cosmic
     inputs.self.desktops.gnome
+    inputs.ragenix.nixosModules.default
+    age
     core
     programs
     services
     catppuccin
     home-manager
     cloud-hypervisor
-    tangled-spindle
+    tangled-spindle-nix
     {
+      age.secrets.spindle-token = {
+        file = inputs.self.secrets.spindle-token;
+        mode = "600";
+      };
+
+      services.tangled-spindles.default = {
+        enable = true;
+        hostname = "spindle.overby.me";
+        owner = "did:plc:eukcx4amfqmhfrnkix7zwm34";
+        tokenFile = "/run/agenix/spindle-token";
+        engine = {
+          maxJobs = 2;
+          queueSize = 100;
+          workflowTimeout = "2h";
+        };
+      };
+
+      services.caddy = {
+        enable = true;
+        virtualHosts."spindle.overby.me" = {
+          extraConfig = ''
+            reverse_proxy localhost:6555
+          '';
+        };
+      };
+
       security.sudo.wheelNeedsPassword = false;
       networking = {
         hostName = "home";
@@ -41,7 +69,7 @@
         };
         defaultGateway = "10.0.0.1";
         nameservers = ["194.242.2.2"];
-        firewall.allowedTCPPorts = [22];
+        firewall.allowedTCPPorts = [22 80 443];
       };
     }
   ];
