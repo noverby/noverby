255e6ae7e70be5b2b90f5298b82c3e668a8e3906
Niclas Overby <niclas@overby.me>
2026-03-14 15:20:28 +0100

    feat(web/wiki): Add Nix package definition using buildDenoProject
    Signed-off-by: Niclas Overby <niclas@overby.me>

--- wiki/default.nix
+++ wiki/default.nix
@@ -5,4 +5,28 @@
       deno
     ];
   };
+
+  packages.wiki-frontend = {lib, ...}:
+    lib.buildDenoProject {
+      pname = "wiki-frontend";
+      src = lib.fileset.toSource {
+        root = ./.;
+        fileset = lib.fileset.unions [
+          ./deno.json
+          ./package.json
+          ./deno.lock
+          ./tsconfig.json
+          ./rsbuild.config.ts
+          ./src
+          ./core
+          ./public
+        ];
+      };
+      buildCommand = ''
+        deno run -A npm:@gqty/cli generate
+        deno run -A npm:@rsbuild/core build
+      '';
+      installPhase = "cp -r dist $out";
+      meta.description = "Wiki frontend built with Deno + RSBuild";
+    };
 }
