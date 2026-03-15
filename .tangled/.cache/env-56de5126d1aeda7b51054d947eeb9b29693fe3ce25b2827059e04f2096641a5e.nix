7283947204988c011977e621adf09add17329502
Niclas Overby <niclas@overby.me>
2026-03-15 10:52:12 +0100

    chore(git): Remove .tangled/.cache and add .cache to .gitignore
    Signed-off-by: Niclas Overby <niclas@overby.me>

--- .tangled/.cache/env-56de5126d1aeda7b51054d947eeb9b29693fe3ce25b2827059e04f2096641a5e.nix
+++
@@ -1,9 +0,0 @@
-let
-  nixpkgs = import (builtins.fetchTarball {url = "https://github.com/NixOS/nixpkgs/archive/nixpkgs-unstable.tar.gz";}) {};
-in
-  nixpkgs.buildEnv {
-    name = "spindle-workflow-env";
-    paths = [
-      nixpkgs.cachix
-    ];
-  }
