4276e6153374b26989974b1a1b24eb476eeff227
Niclas Overby <niclas@overby.me>
2026-03-17 19:34:54 +0100

    feat(web/wiki-dioxus): Add Nix build derivation and update plan
    Signed-off-by: Niclas Overby <niclas@overby.me>

--- web/wiki-dioxus/default.nix
+++ web/wiki-dioxus/default.nix
@@ -4,9 +4,53 @@
       just
       cargo
       rustc
+      rust-analyzer
       dioxus-cli
-      wasm-pack
+      wasm-bindgen-cli
       binaryen
     ];
   };
+
+  packages.wiki-dioxus-frontend = {
+    lib,
+    rustPlatform,
+    dioxus-cli,
+    wasm-bindgen-cli,
+    binaryen,
+    ...
+  }:
+    rustPlatform.buildRustPackage {
+      pname = "wiki-dioxus-frontend";
+      version = "0.1.0";
+
+      src = lib.fileset.toSource {
+        root = ./.;
+        fileset = lib.fileset.unions [
+          ./Cargo.toml
+          ./Cargo.lock
+          ./Dioxus.toml
+          ./src
+          ./assets
+          ./graphql
+        ];
+      };
+
+      cargoLock.lockFile = ./Cargo.lock;
+
+      nativeBuildInputs = [
+        dioxus-cli
+        wasm-bindgen-cli
+        binaryen
+      ];
+
+      buildPhase = ''
+        dx build --release
+      '';
+
+      installPhase = ''
+        cp -r dist $out
+      '';
+
+      meta.description = "RadikalWiki frontend built with Dioxus + Rust/WASM";
+    };
 }
